53b231ff0d908f878cf0528610295c70bfed2d93
DDanDev <110492441+DDanDev@users.noreply.github.com>
2023-01-11 16:09:12 -0300

    even shorter

--- faqExpand.js
+++ faqExpand.js
@@ -2,23 +2,20 @@ const faqItems = document.querySelectorAll(".faqItem");
 const faqTitles = document.querySelectorAll(".faqTitle");
 let faqShown = null;
 
-faqTitles.forEach((faqTitle, faqTitleIndex) => {
-	faqTitle.addEventListener("click", () => faqExpand(faqTitleIndex));
+faqTitles.forEach((faqTitle, showIndex) => {
+	faqTitle.addEventListener("click", () => {
+		faqItems.forEach((faqItem) => faqItem.classList.remove("faqShown"));
+		setTimeout(
+			() => {
+				if (faqShown !== showIndex) {
+					faqItems[showIndex].classList.add("faqShown");
+					faqShown = showIndex;
+					return;
+				} else {
+					faqShown = null;
+				}
+			},
+			faqShown === null ? 0 : 500
+		);
+	});
 });
-
-function faqExpand(showIndex) {
-	faqItems.forEach((faqItem) => faqItem.classList.remove("faqShown"));
-
-	setTimeout(
-		() => {
-			if (faqShown !== showIndex) {
-				faqItems[showIndex].classList.add("faqShown");
-				faqShown = showIndex;
-				return;
-			} else {
-				faqShown = null;
-			}
-		},
-		faqShown === null ? 0 : 500
-	);
-}
